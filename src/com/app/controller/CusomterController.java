2cc3f786e7a65e4d6be8671863accf6937fb3f27
rs <rs@DESKTOP-V0HG1DG>
2018-09-05 20:15:28 +0530

    edit customer controller

--- src/com/app/controller/CusomterController.java
+++ src/com/app/controller/CusomterController.java
@@ -38,5 +38,55 @@ public class CusomterController {
 		return "CustomerReg";
 	}
 
-	
+	@RequestMapping("/viewAllCusts")
+	public String getAllCusts(ModelMap map){
+		List<Customer> cust=service.getAllCustomers();
+		map.addAttribute("customers", cust);
+		return "CustomerData";
+	}
+	@RequestMapping("/deleteCust")
+	public String deleteCustomer(@RequestParam("custId")int custId){
+		service.deleteCustomer(custId);
+		return "redirect:viewAllCusts";
+	}
+
+	@RequestMapping("/editCust")
+	public String CustEdit(@RequestParam("custId")int custId,ModelMap map){
+		Customer cust=service.getCustomerById(custId);
+		map.addAttribute("cust", cust);
+		return "CustomerDataEdit";
+	}
+
+	@RequestMapping(value="/updateCust",method=RequestMethod.POST)
+	public String updateCustomer(@ModelAttribute("customer")Customer cust){
+		service.updateCustomer(cust);
+		return "redirect:viewAllCusts";
+	}
+	/* 7. This method is used to ExcelExport*/
+	@RequestMapping("/custExcelExport")
+	public String showExcelData(ModelMap map){
+		List<Customer> custs=service.getAllCustomers();
+		map.addAttribute("custs",custs);
+		return "custExcelView";
+	}
+
+	/* 8.Location PDF Data export*/ 
+	@RequestMapping("/custPdfExport")
+	public String PdfData(ModelMap map){
+		List<Customer> custs=service.getAllCustomers();
+		map.addAttribute("custs",custs);
+		return "custPdfView";
+	}
+
+	/* 9. Report Design*/
+	@RequestMapping("/showCustReports")
+	public String generateReports(){
+		List<Object[]> list=service.getCustomerTypeCount();
+		String path=context.getRealPath("/");
+		util.generatePieChart(path, list);
+		util.generateBarChart(path, list);
+		return "CustomerReports";
+	}
+
+
 }
